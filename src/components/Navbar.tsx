5e7305391ce1418ef57b4e3b35f8ecf6df0c42e0
Nemidas <nehalchaudhari5302@gmail.com>
2024-04-21 01:03:43 +0530

    Initial commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,10 @@
+export default function Navbar(){
+    return (
+        <>
+        <div className="flex items-center">
+            <img src="35322171_8_1sasa11.svg" width={65} height={65} className="inline"/>
+            <span className="text-3xl font-bold">ChirSense</span>
+        </div>
+        </>
+    )
+}
